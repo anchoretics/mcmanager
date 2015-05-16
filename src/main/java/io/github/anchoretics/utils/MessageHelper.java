04bb01bb5b031ca19219765f23f1c7adff592253
anchoretic <anchoretic@vip.qq.com>
2015-05-16 10:35:15 +0800

    m msg type

--- src/main/java/io/github/anchoretics/utils/MessageHelper.java
+++ src/main/java/io/github/anchoretics/utils/MessageHelper.java
@@ -1,17 +1,15 @@
 package io.github.anchoretics.utils;
 
-import io.github.anchoretics.MsgType;
-
 import org.bukkit.entity.Player;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MessageHelper {
 
-	public static JSONObject getJsonMessage(MsgType.Type type,String msg, Player player){
+	public static JSONObject getJsonMessage(String type,String msg, Player player){
 		JSONObject jo = PlayerHelper.getPlayerJson(player);
 		try {
-			jo.put("type", type.name().toLowerCase());
+			jo.put("type", type);
 			jo.put("msg", msg);
 		} catch (JSONException e) {
 			e.printStackTrace();
